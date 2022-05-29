935c4b83fe3d97d8e36d21f011bf6712bf8a95c3
Julien Giraud <giraud740@gmail.com>
2022-05-29 14:48:14 +0200

    hash + clean bruit

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import crepe
 import os
+import glob
 from flask import Flask, request, send_file
 from flask_cors import CORS
 import base64
+import random
+import string
 
 
 app = Flask(__name__)
 CORS(app)
+
+
+def delete_files(start):
+    files = glob.glob(start)
+    for file in files:
+        if file is None:
+            continue
+        try:
+            os.remove(file)
+        except:
+            print("Error while deleting file : ", file)
+
+
+delete_files('tmp_*')
 
 
 @app.route('/')
@@ -14,26 +31,34 @@ def index():
     return ''
 
 
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str;
+
+
 @app.route('/', methods=['POST'])
 # request.data = wave file as byte array
 def upload_file():
 
-    f = open('audio', 'wb')
+    hash = 'tmp_' + get_random_string(8) + '-'
+    f = open(hash + 'audio', 'wb')
     f.write(request.data)
 
-    with open('audio', 'r') as input_file:
+    with open(hash + 'audio', 'r') as input_file:
       coded_string = input_file.read()
     decoded = base64.b64decode(coded_string)
-    with open('audio_decoded', 'wb') as output_file:
+    with open(hash + 'audio_decoded', 'wb') as output_file:
       output_file.write(decoded)
     try:
-        os.remove('audio.wav')
+        os.remove(hash + 'audio.wav')
     except:
         None
-    os.system('ffmpeg -i audio_decoded audio.wav')
+    os.system('ffmpeg -i ' + hash + 'audio_decoded ' + hash + 'audio.wav')
+    os.system('./noiseclean.sh ' + hash + 'audio.wav ' + hash + 'result.wav')
 
     crepe.process_file(
-        file='audio.wav',
+        file=hash + 'result.wav',
         # output='',  # output directory
         model_capacity='medium',  # medium, full
         viterbi=True,  # ?
@@ -44,5 +69,5 @@ def upload_file():
         step_size=10,
         verbose=True
     )
-    image = 'audio.activation.png'
+    image = hash + 'result.activation.png'
     return send_file(image, attachment_filename=image)
